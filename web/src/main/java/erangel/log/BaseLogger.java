e634bc7c725179cca2d7a0fa9876a6b51394f2d5
LILINJIAN <shinozaki9852@gmail.com>
2024-11-26 17:36:45 +0900

    Added logging framework

---
+++ web/src/main/java/erangel/log/BaseLogger.java
@@ -0,0 +1,10 @@
+package erangel.log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 提供日志记录功能。
+ */
+public abstract class BaseLogger {
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
