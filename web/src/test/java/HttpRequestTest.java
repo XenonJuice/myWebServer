2b2223efe24112779f93fc0d878c8e607a8c6d7b
xenonJuice <myEmail>
2024-11-27 16:26:53 +0900

    do some tests.

--- web/src/test/java/HttpRequestTest.java
+++ web/src/test/java/HttpRequestTest.java
@@ -1,6 +1,5 @@
 
 import erangel.connector.http.HttpRequest;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
