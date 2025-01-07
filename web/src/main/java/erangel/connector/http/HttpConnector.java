39ba9f778f55d3f1e9cecaa2fd211cea489e8ced
xenonJuice <jiisennjyounomaou@163.com>
2025-01-07 20:24:02 +0900

    some methods related to the socket factory has been added to the Http connector.

--- web/src/main/java/erangel/connector/http/HttpConnector.java
+++ web/src/main/java/erangel/connector/http/HttpConnector.java
@@ -1,14 +1,47 @@
 package erangel.connector.http;
 
+import erangel.net.DefaultServerSocketFactory;
+import erangel.net.ServerSocketFactory;
+
+import java.net.ServerSocket;
+
 public class HttpConnector implements Runnable {
+    private ServerSocketFactory factory = null;
+
     @Override
     public void run() {
 
     }
 
-    void threadStart() {}
-    void threadStop() {}
-    void initalize() {}
-    void start() {}
-    void stop() {}
+    public ServerSocketFactory getFactory() {
+        if (factory == null) {
+            synchronized (this) {
+                factory = new DefaultServerSocketFactory();
+            }
+        }
+        return (factory);
+    }
+
+    public void setFactory(ServerSocketFactory factory) {
+        this.factory = factory;
+    }
+
+    private ServerSocket openSocket() {
+        return null;
+    }
+
+    void threadStart() {
+    }
+
+    void threadStop() {
+    }
+
+    void initalize() {
+    }
+
+    void start() {
+    }
+
+    void stop() {
+    }
 }
