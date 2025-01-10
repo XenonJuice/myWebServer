0c742fd62709ef75595bb371b694054664a34442
xenonJuice <jiisennjyounomaou@163.com>
2025-01-10 16:28:08 +0900

    created a constant class

---
+++ web/src/main/java/erangel/connector/http/Const.java
@@ -0,0 +1,13 @@
+package erangel.connector.http;
+
+public final class Const {
+    public static class Package {
+        public static final String Package = "llj.erangel.connector.http";
+    }
+
+    public static class Processor {
+        public static final int DEFAULT_CONNECTION_TIMEOUT = 60000;
+        public static final int PROCESSOR_IDLE = 0;
+        public static final int PROCESSOR_ACTIVE = 1;
+    }
+}
