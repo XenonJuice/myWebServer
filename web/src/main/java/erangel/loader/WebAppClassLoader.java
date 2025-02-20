a6a82a067672e4ef444805d8264c7367a2f3d4b8
LILINJIAN <shinozaki9852@gmail.com>
2025-02-20 17:58:54 +0900

    Extend WebAppClassLoader to inherit from URLClassLoader

--- web/src/main/java/erangel/loader/WebAppClassLoader.java
+++ web/src/main/java/erangel/loader/WebAppClassLoader.java
@@ -1,4 +1,11 @@
 package erangel.loader;
 
-public class WebAppClassLoader {
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class WebAppClassLoader extends URLClassLoader {
+    // TODO
+    public WebAppClassLoader(String name, URL[] urls, ClassLoader parent) {
+        super(name, urls, parent);
+    }
 }
