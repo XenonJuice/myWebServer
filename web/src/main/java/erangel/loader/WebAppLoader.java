61642d27878ce670fb4142ad777baf83ea0dcbe4
LILINJIAN <shinozaki9852@gmail.com>
2025-02-21 17:36:24 +0900

    Refactor WebAppLoader class organization and comments.

--- web/src/main/java/erangel/loader/WebAppLoader.java
+++ web/src/main/java/erangel/loader/WebAppLoader.java
@@ -5,27 +5,26 @@ import erangel.Loader;
 import erangel.Vas;
 
 public class WebAppLoader implements Loader {
+    // 当前loader所处容器
     private Vas vas = null;
+    // 与此loader所过连的web上下文
     private Context context = null;
+    // 是否遵循默认委派模型
     private boolean delegate = false;
+    // 是否可重载
     private boolean reloadable = false;
+    // 此组件的父加载器
     private ClassLoader parentClassLoader = null;
+    // 被这个loader组件管理的真正的加载器
     private WebAppClassLoader webAppClassLoader = null;
+    // 使用的类加载器的全限定名
+    private String loadClass = WebAppClassLoader.class.getName();
 
-    public String getLoadClass() {
-        return this.loadClass;
-    }
-
-    public void setLoadClass(String loadClass) {
-        this.loadClass = loadClass;
-    }
-
-    // TODO add a suitable class loader
-    private String loadClass = null;
-
+    // 默认构造器 调用可以传入一个父加载器的构造器
     public WebAppLoader() {
         this(null);
     }
+    // 传入一个父加载器的构造器
     public WebAppLoader(ClassLoader parent) {
         this.parentClassLoader = parent;
     }
@@ -81,6 +80,14 @@ public class WebAppLoader implements Loader {
 
     }
 
+    public String getLoadClass() {
+        return this.loadClass;
+    }
+
+    public void setLoadClass(String loadClass) {
+        this.loadClass = loadClass;
+    }
+
     @Override
     public String[] findRepositories() {
         return new String[0];
@@ -90,6 +97,8 @@ public class WebAppLoader implements Loader {
     public boolean modified() {
         return false;
     }
+
+
 
     private void sleep(){
         try {
