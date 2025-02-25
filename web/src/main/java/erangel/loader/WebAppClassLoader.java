f47aa30e3a8d45ba1b3cc62e78b32d3a536c96de
LILINJIAN <shinozaki9852@gmail.com>
2025-02-25 15:42:32 +0900

    Add some test method and constructors to WebAppClassLoader

--- web/src/main/java/erangel/loader/WebAppClassLoader.java
+++ web/src/main/java/erangel/loader/WebAppClassLoader.java
@@ -8,4 +8,24 @@ public class WebAppClassLoader extends URLClassLoader {
     public WebAppClassLoader(String name, URL[] urls, ClassLoader parent) {
         super(name, urls, parent);
     }
+    public static void main(String[] args) {
+        String fullyQualifiedName = WebAppClassLoader.class.getName();
+        System.out.println("当前类的全限定名：" + fullyQualifiedName);
+
+        // 获取当前方法的名称
+        String currentMethodName = new Throwable().getStackTrace()[0].getMethodName();
+        System.out.println("当前方法的名称：" + currentMethodName);
+
+        // 获取当前方法所在类的全限定名
+        String currentClassName = new Throwable().getStackTrace()[0].getClassName();
+        System.out.println("当前方法所在类的全限定名：" + currentClassName);
+    }
+    public WebAppClassLoader() {
+        super(new URL[0]);
+    }
+    public WebAppClassLoader(ClassLoader parent) {
+        super(new URL[0], parent);
+    }
+
+
 }
