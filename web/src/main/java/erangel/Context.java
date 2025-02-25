9072e3f06a2b0180d30bab5a932b7a6a3bce900b
LILINJIAN <shinozaki9852@gmail.com>
2025-02-25 17:58:30 +0900

    Add methods to Context interface for reloadable flag and ServletContext

--- web/src/main/java/erangel/Context.java
+++ web/src/main/java/erangel/Context.java
@@ -1,11 +1,21 @@
 package erangel;
+
+import javax.servlet.ServletContext;
+
 /**
  * 代表一个Web程序上下文
- * <p>
- * <p>
  *
  * @author LILINJIAN
  * @version 2025/02/20
  */
 public interface Context extends Vas {
+    /**
+     * 取得是否可重载标志位
+     */
+    boolean getReloadable();
+
+    /**
+     * 返回servlet上下文。
+     */
+    ServletContext getServletContext();
 }
