2f858d72d92df3a7547c3633d7e5de0e4498a439
LILINJIAN <shinozaki9852@gmail.com>
2025-02-26 17:59:30 +0900

    Refactor abstractDirContext class and add new methods

--- web/src/main/java/erangel/webResource/abstractDirContext.java
+++ web/src/main/java/erangel/webResource/abstractDirContext.java
@@ -11,7 +11,7 @@ import java.util.Map;
  * @author LILINJIAN
  * @version 2025/2/26
  */
-public class abstractDirContext implements DirContext {
+public abstract class abstractDirContext implements DirContext {
 
     //<editor-fold desc = "构造器">
     public abstractDirContext() {
@@ -26,15 +26,79 @@ public class abstractDirContext implements DirContext {
     protected  Map<String, Object> bindings = new HashMap<>();
     // 创建DirContext时，可传入一些环境信息
     protected  Hashtable<?, ?> environment =null;
+    // 文件根目录
+    protected String root = "";
     //
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "getter&setter">
+    public String getRoot() {
+        return root;
+    }
+
+    public void setRoot(String root) {
+        if (root == null) {
+            throw new NullPointerException("root is null");
+        }
+        this.root = root;
+    }
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "抽象方法">
+    public abstract Object lookup(String name) throws NamingException;
+    public abstract void bind(String name, Object obj, Attributes attrs) throws NamingException;
+    public abstract void rebind(String name, Object obj, Attributes attrs) throws NamingException;
+    public abstract void unbind(String name) throws NamingException;
+    public abstract void rename(String oldName, String newName) throws NamingException;
+    public abstract NamingEnumeration<NameClassPair> list(String name) throws NamingException;
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "方法重载的源方法">
+    @Override
+    public Object lookup(Name name) throws NamingException {
+        return lookup(name.toString());
+    }
+    @Override
+    public void bind(Name name, Object obj)
+            throws NamingException {
+        bind(name.toString(), obj);
+    }
+    @Override
+    public void bind(String name, Object obj)
+            throws NamingException {
+        bind(name, obj, null);
+    }
+    @Override
+    public void bind(Name name, Object obj, Attributes attrs)
+            throws NamingException {
+        bind(name.toString(), obj, attrs);
+    }
+    public void rebind(Name name, Object obj) throws NamingException {
+        rebind(name.toString(), obj);
+    }
+    public void rebind(String name, Object obj) throws NamingException {
+        rebind(name, obj, null);
+    }
+    public void unbind(Name name) throws NamingException {
+        unbind(name.toString());
+    }
+    public void rename(Name oldName, Name newName)
+            throws NamingException {
+        rename(oldName.toString(), newName.toString());
+    }
+    @Override
+    public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
+        return null;
+    }
+
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "不实现">
+    @Override
+    public DirContext getSchema(Name name) throws NamingException {
+        return null;
+    }
+
+    @Override
+    public DirContext getSchema(String name) throws NamingException {
+        return null;
+    }
     //</editor-fold>
     @Override
     public Attributes getAttributes(Name name) throws NamingException {
@@ -77,22 +141,7 @@ public class abstractDirContext implements DirContext {
     }
 
     @Override
-    public void bind(Name name, Object obj, Attributes attrs) throws NamingException {
-
-    }
-
-    @Override
-    public void bind(String name, Object obj, Attributes attrs) throws NamingException {
-
-    }
-
-    @Override
     public void rebind(Name name, Object obj, Attributes attrs) throws NamingException {
-
-    }
-
-    @Override
-    public void rebind(String name, Object obj, Attributes attrs) throws NamingException {
 
     }
 
@@ -106,15 +155,7 @@ public class abstractDirContext implements DirContext {
         return null;
     }
 
-    @Override
-    public DirContext getSchema(Name name) throws NamingException {
-        return null;
-    }
 
-    @Override
-    public DirContext getSchema(String name) throws NamingException {
-        return null;
-    }
 
     @Override
     public DirContext getSchemaClassDefinition(Name name) throws NamingException {
@@ -166,65 +207,7 @@ public class abstractDirContext implements DirContext {
         return null;
     }
 
-    @Override
-    public Object lookup(Name name) throws NamingException {
-        return null;
-    }
 
-    @Override
-    public Object lookup(String name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public void bind(Name name, Object obj) throws NamingException {
-
-    }
-
-    @Override
-    public void bind(String name, Object obj) throws NamingException {
-
-    }
-
-    @Override
-    public void rebind(Name name, Object obj) throws NamingException {
-
-    }
-
-    @Override
-    public void rebind(String name, Object obj) throws NamingException {
-
-    }
-
-    @Override
-    public void unbind(Name name) throws NamingException {
-
-    }
-
-    @Override
-    public void unbind(String name) throws NamingException {
-
-    }
-
-    @Override
-    public void rename(Name oldName, Name newName) throws NamingException {
-
-    }
-
-    @Override
-    public void rename(String oldName, String newName) throws NamingException {
-
-    }
-
-    @Override
-    public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<NameClassPair> list(String name) throws NamingException {
-        return null;
-    }
 
     @Override
     public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
