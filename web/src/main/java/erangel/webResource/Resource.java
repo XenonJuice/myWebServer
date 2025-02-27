442649affbe1525b110026c1f8d5ece74680e0ac
LILINJIAN <shinozaki9852@gmail.com>
2025-02-27 17:58:37 +0900

    Add support for binary content handling in Resource class

--- web/src/main/java/erangel/webResource/Resource.java
+++ web/src/main/java/erangel/webResource/Resource.java
@@ -1,10 +1,12 @@
 package erangel.webResource;
 
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * 封装web资源，变成流的形式
+ * 封装web资源，变成流或者二进制的形式
  * @author LILINJIAN
  * @version 2025/2/26
  */
@@ -18,16 +20,24 @@ public class Resource {
     //</editor-fold>
     //<editor-fold desc = "attr">
     protected InputStream inputStream = null;
+    protected byte[] binaryContent = null;
     //</editor-fold>
     //<editor-fold desc = "其他方法">
-    public InputStream getContent() {
+    //</editor-fold>
+    //<editor-fold desc = "getter&setter">
+    public InputStream getStreamContent() throws IOException {
+        if (binaryContent != null) return new ByteArrayInputStream(binaryContent);
         return inputStream;
+    }
+    public byte[] getBinaryContent() {
+        return binaryContent;
     }
     public void setContent(InputStream inputStream) {
         this.inputStream = inputStream;
     }
-    //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    public void setContent(byte[] binaryContent) {
+        this.binaryContent = binaryContent;
+    }
     //</editor-fold>
     //<editor-fold desc = "XXXXXXX">
     //</editor-fold>
