7205ad6db35702a1ae0d42af91d696912f9e3574
LILINJIAN <shinozaki9852@gmail.com>
2025-02-27 13:58:11 +0900

    Refactor abstractDirContext for enhanced functionality

--- web/src/main/java/erangel/webResource/abstractDirContext.java
+++ web/src/main/java/erangel/webResource/abstractDirContext.java
@@ -1,5 +1,7 @@
 package erangel.webResource;
 
+import erangel.Const;
+
 import javax.naming.*;
 import javax.naming.directory.*;
 import java.util.HashMap;
@@ -8,27 +10,30 @@ import java.util.Map;
 
 /**
  * DirContext的基础实现
+ *
  * @author LILINJIAN
  * @version 2025/2/26
  */
 public abstract class abstractDirContext implements DirContext {
-
-    //<editor-fold desc = "构造器">
-    public abstractDirContext() {
-        this.environment = new Hashtable<>();
-    }
-    public abstractDirContext(Hashtable<?, ?> environment) {
-        this.environment = environment;
-    }
-    //</editor-fold>
     //<editor-fold desc = "attr">
+
     // 用来存储绑定的对象
-    protected  Map<String, Object> bindings = new HashMap<>();
+    protected Map<String, Object> bindings = new HashMap<>();
     // 创建DirContext时，可传入一些环境信息
-    protected  Hashtable<?, ?> environment =null;
+    protected Hashtable<String, Object> environment = null;
     // 文件根目录
     protected String root = "";
-    //
+
+    //</editor-fold>
+    //<editor-fold desc = "构造器">
+    public abstractDirContext() {
+        this.environment = new Hashtable<String, Object>();
+    }
+
+    public abstractDirContext(Hashtable<String, Object> environment) {
+        this.environment = environment;
+    }
+
     //</editor-fold>
     //<editor-fold desc = "getter&setter">
     public String getRoot() {
@@ -41,257 +46,220 @@ public abstract class abstractDirContext implements DirContext {
         }
         this.root = root;
     }
+
     //</editor-fold>
     //<editor-fold desc = "抽象方法">
     public abstract Object lookup(String name) throws NamingException;
+
     public abstract void bind(String name, Object obj, Attributes attrs) throws NamingException;
+
     public abstract void rebind(String name, Object obj, Attributes attrs) throws NamingException;
+
     public abstract void unbind(String name) throws NamingException;
+
     public abstract void rename(String oldName, String newName) throws NamingException;
+
     public abstract NamingEnumeration<NameClassPair> list(String name) throws NamingException;
+
+    public abstract NamingEnumeration<Binding> listBindings(String name) throws NamingException;
+
+    public abstract void destroySubcontext(String name) throws NamingException;
+
+    public abstract DirContext createSubcontext(String name, Attributes attrs) throws NamingException;
+
+    public abstract DirContext getSchema(String name) throws NamingException;
+
+    public abstract DirContext getSchemaClassDefinition(String name) throws NamingException;
+
+    public abstract NamingEnumeration<SearchResult> search(String name, Attributes matchingAttributes) throws NamingException;
+
+    public abstract NamingEnumeration<SearchResult> search(String name, String filterExpr, Object[] filterArgs, SearchControls cons) throws NamingException;
+
+    public abstract NamingEnumeration<SearchResult> search(String name, Attributes matchingAttributes, String[] attributesToReturn) throws NamingException;
+
+    public abstract NamingEnumeration<SearchResult> search(String name, String filter, SearchControls cons) throws NamingException;
+
+    public abstract Attributes getAttributes(String name, String[] attrIds) throws NamingException;
+
+    public abstract void modifyAttributes(String name, int mod_op, Attributes attrs) throws NamingException;
+
+    public abstract void modifyAttributes(String name, ModificationItem[] mods) throws NamingException;
+
+    public abstract String getNameInNamespace() throws NamingException;
+
     //</editor-fold>
     //<editor-fold desc = "方法重载的源方法">
     @Override
     public Object lookup(Name name) throws NamingException {
         return lookup(name.toString());
     }
+
     @Override
     public void bind(Name name, Object obj)
             throws NamingException {
         bind(name.toString(), obj);
     }
+
     @Override
     public void bind(String name, Object obj)
             throws NamingException {
         bind(name, obj, null);
     }
+
     @Override
     public void bind(Name name, Object obj, Attributes attrs)
             throws NamingException {
         bind(name.toString(), obj, attrs);
     }
+
     public void rebind(Name name, Object obj) throws NamingException {
         rebind(name.toString(), obj);
     }
+
     public void rebind(String name, Object obj) throws NamingException {
         rebind(name, obj, null);
     }
+
+    public void rebind(Name name, Object obj, Attributes attrs)
+            throws NamingException {
+        rebind(name.toString(), obj, attrs);
+    }
+
     public void unbind(Name name) throws NamingException {
         unbind(name.toString());
     }
+
     public void rename(Name oldName, Name newName)
             throws NamingException {
         rename(oldName.toString(), newName.toString());
     }
-    @Override
+
     public NamingEnumeration<NameClassPair> list(Name name) throws NamingException {
-        return null;
+        return list(name.toString());
     }
 
-    //</editor-fold>
-    //<editor-fold desc = "不实现">
-    @Override
-    public DirContext getSchema(Name name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public DirContext getSchema(String name) throws NamingException {
-        return null;
-    }
-    //</editor-fold>
-    @Override
-    public Attributes getAttributes(Name name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public Attributes getAttributes(String name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public Attributes getAttributes(Name name, String[] attrIds) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public Attributes getAttributes(String name, String[] attrIds) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public void modifyAttributes(Name name, int mod_op, Attributes attrs) throws NamingException {
-
-    }
-
-    @Override
-    public void modifyAttributes(String name, int mod_op, Attributes attrs) throws NamingException {
-
-    }
-
-    @Override
-    public void modifyAttributes(Name name, ModificationItem[] mods) throws NamingException {
-
-    }
-
-    @Override
-    public void modifyAttributes(String name, ModificationItem[] mods) throws NamingException {
-
-    }
-
-    @Override
-    public void rebind(Name name, Object obj, Attributes attrs) throws NamingException {
-
-    }
-
-    @Override
-    public DirContext createSubcontext(Name name, Attributes attrs) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public DirContext createSubcontext(String name, Attributes attrs) throws NamingException {
-        return null;
-    }
-
-
-
-    @Override
-    public DirContext getSchemaClassDefinition(Name name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public DirContext getSchemaClassDefinition(String name) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(Name name, Attributes matchingAttributes, String[] attributesToReturn) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(String name, Attributes matchingAttributes, String[] attributesToReturn) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(Name name, Attributes matchingAttributes) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(String name, Attributes matchingAttributes) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(Name name, String filter, SearchControls cons) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(String name, String filter, SearchControls cons) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(Name name, String filterExpr, Object[] filterArgs, SearchControls cons) throws NamingException {
-        return null;
-    }
-
-    @Override
-    public NamingEnumeration<SearchResult> search(String name, String filterExpr, Object[] filterArgs, SearchControls cons) throws NamingException {
-        return null;
-    }
-
-
-
-    @Override
     public NamingEnumeration<Binding> listBindings(Name name) throws NamingException {
-        return null;
+        return listBindings(name.toString());
     }
 
-    @Override
-    public NamingEnumeration<Binding> listBindings(String name) throws NamingException {
-        return null;
+    public void destroySubcontext(Name name)
+            throws NamingException {
+        destroySubcontext(name.toString());
     }
 
-    @Override
-    public void destroySubcontext(Name name) throws NamingException {
-
+    public Context createSubcontext(Name name)
+            throws NamingException {
+        return createSubcontext(name.toString());
     }
 
-    @Override
-    public void destroySubcontext(String name) throws NamingException {
-
+    public Context createSubcontext(String name)
+            throws NamingException {
+        return createSubcontext(name, null);
     }
 
-    @Override
-    public Context createSubcontext(Name name) throws NamingException {
-        return null;
+    public DirContext createSubcontext(Name name, Attributes attrs)
+            throws NamingException {
+        return createSubcontext(name.toString(), attrs);
     }
 
-    @Override
-    public Context createSubcontext(String name) throws NamingException {
-        return null;
+    public DirContext getSchema(Name name)
+            throws NamingException {
+        return getSchema(name.toString());
     }
 
-    @Override
-    public Object lookupLink(Name name) throws NamingException {
-        return null;
+    public DirContext getSchemaClassDefinition(Name name)
+            throws NamingException {
+        return getSchemaClassDefinition(name.toString());
     }
 
-    @Override
-    public Object lookupLink(String name) throws NamingException {
-        return null;
+    public NamingEnumeration<SearchResult> search(Name name, Attributes matchingAttributes, String[] attributesToReturn)
+            throws NamingException {
+        return search(name.toString(), matchingAttributes, attributesToReturn);
     }
 
-    @Override
+    public NamingEnumeration<SearchResult> search(Name name, Attributes matchingAttributes)
+            throws NamingException {
+        return search(name.toString(), matchingAttributes);
+    }
+
+    public NamingEnumeration<SearchResult> search
+            (Name name, String filter, SearchControls cons)
+            throws NamingException {
+        return search(name.toString(), filter, cons);
+    }
+
+    public NamingEnumeration<SearchResult> search(Name name, String filterExpr, Object[] filterArgs, SearchControls cons)
+            throws NamingException {
+        return search(name.toString(), filterExpr, filterArgs, cons);
+    }
+
+    public Attributes getAttributes(Name name)
+            throws NamingException {
+        return getAttributes(name.toString());
+    }
+
+    public Attributes getAttributes(String name)
+            throws NamingException {
+        return getAttributes(name, null);
+    }
+
+    public Attributes getAttributes(Name name, String[] attrIds)
+            throws NamingException {
+        return getAttributes(name.toString(), attrIds);
+    }
+
+    public void modifyAttributes(Name name, int mod_op, Attributes attrs)
+            throws NamingException {
+        modifyAttributes(name.toString(), mod_op, attrs);
+    }
+
+    public void modifyAttributes(Name name, ModificationItem[] mods)
+            throws NamingException {
+        modifyAttributes(name.toString(), mods);
+    }
+
+    // 将单独的名称转换为一个名称组件
     public NameParser getNameParser(Name name) throws NamingException {
-        return null;
+        return n -> new CompositeName(n);
     }
 
-    @Override
     public NameParser getNameParser(String name) throws NamingException {
-        return null;
+        return n -> new CompositeName(n);
     }
 
-    @Override
+    // 拼接为完整的名字
     public Name composeName(Name name, Name prefix) throws NamingException {
-        return null;
+        prefix = (Name) name.clone();
+        return prefix.addAll(name);
     }
 
-    @Override
     public String composeName(String name, String prefix) throws NamingException {
-        return "";
+        return prefix + Const.commonCharacters.SOLIDUS + name;
     }
 
     @Override
     public Object addToEnvironment(String propName, Object propVal) throws NamingException {
-        return null;
+        return environment.put(propName, propVal);
     }
 
     @Override
     public Object removeFromEnvironment(String propName) throws NamingException {
-        return null;
+        return environment.remove(propName);
     }
 
     @Override
     public Hashtable<?, ?> getEnvironment() throws NamingException {
-        return null;
+        return environment;
     }
 
     @Override
     public void close() throws NamingException {
-
+        environment.clear();
     }
+    //</editor-fold>
 
-    @Override
-    public String getNameInNamespace() throws NamingException {
-        return "";
-    }
+    //<editor-fold desc = "不实现">
+    //</editor-fold>
+
 
 }
