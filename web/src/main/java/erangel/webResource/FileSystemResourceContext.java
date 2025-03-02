aab9ef73f76647a96be7e71a9d0a9263e2b8c5ec
LILINJIAN <shinozaki9852@gmail.com>
2025-03-03 00:13:10 +0900

    Add file modification check to FileSystemResourceContext

--- web/src/main/java/erangel/webResource/FileSystemResourceContext.java
+++ web/src/main/java/erangel/webResource/FileSystemResourceContext.java
@@ -1,18 +1,23 @@
 package erangel.webResource;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 
 /**
  * 封装web资源
+ *
  * @author LILINJIAN
  * @version 2025/2/28
  */
 public class FileSystemResourceContext implements ResourceContext {
     private final Path root;
+    private long lastModified = 0;
 
     public FileSystemResourceContext(Path root) {
         this.root = root;
@@ -34,5 +39,36 @@ public class FileSystemResourceContext implements ResourceContext {
             }
         }
         return resourceList;
+    }
+
+    /**
+     * 检查文件资源自上次检查以来是否已被修改。
+     *
+     * @return 如果资源已被修改则返回 true， 否则返回 false
+     * @throws IOException 如果在访问文件系统时发生 I/O 错误
+     */
+    @Override
+    public boolean isModified() throws IOException {
+        long currentLatest = getLatestModified(root.toFile());
+        return currentLatest != this.lastModified;
+    }
+
+    /**
+     * 递归遍历文件和子目录，返回其中最新的修改时间
+     */
+    private long getLatestModified(File file) {
+        long latest = file.lastModified();
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File child : files) {
+                    long childModified = getLatestModified(child);
+                    if (childModified > latest) {
+                        latest = childModified;
+                    }
+                }
+            }
+        }
+        return latest;
     }
 }
