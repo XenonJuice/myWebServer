f95c128a6f3f5aaac22ccd5afeeee27c27baae8a
LILINJIAN <shinozaki9852@gmail.com>
2025-03-04 00:06:34 +0900

    Add constants for left and right square brackets

--- web/src/main/java/erangel/Const.java
+++ web/src/main/java/erangel/Const.java
@@ -99,6 +99,8 @@ public final class Const {
         public static final String SEMICOLON = ";";
         public static final String COMMA = ",";
         public static final String COLON_SPACE = ": ";
+        public static final String LEFT_BRACKET = "[";
+        public static final String RIGHT_BRACKET = "]";
     }
 
     public static class CharPunctuationMarks {
