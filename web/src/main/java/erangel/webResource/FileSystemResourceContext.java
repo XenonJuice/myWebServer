79d5af5a1f600c07771dc502dcfb3e174c9a6528
LILINJIAN <shinozaki9852@gmail.com>
2025-03-05 17:55:44 +0900

    Remove file modification tracking and related logic

--- web/src/main/java/erangel/webResource/FileSystemResourceContext.java
+++ web/src/main/java/erangel/webResource/FileSystemResourceContext.java
@@ -1,6 +1,5 @@
 package erangel.webResource;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -17,7 +16,6 @@ import java.util.List;
  */
 public class FileSystemResourceContext implements ResourceContext {
     private final Path root;
-    private long lastModified = 0;
 
     public FileSystemResourceContext(Path root) {
         this.root = root;
@@ -25,8 +23,7 @@ public class FileSystemResourceContext implements ResourceContext {
 
     @Override
     public Path getResource(String path) {
-        // 去除开头的 /
-        return root.resolve(path.substring(1));
+        return root.resolve(path);
     }
 
     @Override
@@ -39,36 +36,5 @@ public class FileSystemResourceContext implements ResourceContext {
             }
         }
         return resourceList;
-    }
-
-    /**
-     * 检查文件资源自上次检查以来是否已被修改。
-     *
-     * @return 如果资源已被修改则返回 true， 否则返回 false
-     * @throws IOException 如果在访问文件系统时发生 I/O 错误
-     */
-    @Override
-    public boolean isModified() throws IOException {
-        long currentLatest = getLatestModified(root.toFile());
-        return currentLatest != this.lastModified;
-    }
-
-    /**
-     * 递归遍历文件和子目录，返回其中最新的修改时间
-     */
-    private long getLatestModified(File file) {
-        long latest = file.lastModified();
-        if (file.isDirectory()) {
-            File[] files = file.listFiles();
-            if (files != null) {
-                for (File child : files) {
-                    long childModified = getLatestModified(child);
-                    if (childModified > latest) {
-                        latest = childModified;
-                    }
-                }
-            }
-        }
-        return latest;
     }
 }
