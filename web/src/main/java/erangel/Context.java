fca18a47feb8c185701f1d16e881f3069d3bd5c2
LILINJIAN <shinozaki9852@gmail.com>
2025-03-06 22:57:34 +0900

    Add base path methods to Context interface

--- web/src/main/java/erangel/Context.java
+++ web/src/main/java/erangel/Context.java
@@ -10,6 +10,16 @@ import javax.servlet.ServletContext;
  */
 public interface Context extends Vas {
     /**
+     * 获取资源的根目录
+     */
+    String getBasePath();
+
+    /**
+     * 设置资源的根目录
+     */
+    String setRealPath(String path);
+
+    /**
      * 取得是否可重载标志位
      */
     boolean getReloadable();
