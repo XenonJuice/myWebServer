9282e63ede2c9262396a292336a78c01525e391a
LILINJIAN <shinozaki9852@gmail.com>
2025-03-09 23:28:54 +0900

    Add error handling and new utility methods to FileResource

--- web/src/main/java/erangel/Resource/FileResource.java
+++ web/src/main/java/erangel/Resource/FileResource.java
@@ -1,15 +1,19 @@
 package erangel.Resource;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import static erangel.Const.webApp.DOTCLASS;
+
 /**
  * {@link LocalResource} 接口的一个具体实现
  * 表示一个基于文件的资源，使用 {@link Path} 来定位该文件。
  * （针对直接存放在 WEB-INF/classes 中的文件）
+ *
  * @author LILINJIAN
  * @version 2025/3/6
  */
@@ -27,8 +31,22 @@ public class FileResource implements LocalResource {
     }
 
     @Override
-    public byte[] getContent() throws IOException {
-        return Files.readAllBytes(path);
+    public boolean isDirectory() {
+        return Files.isDirectory(path);
+    }
+
+    @Override
+    public boolean canRead() {
+        return Files.isReadable(path);
+    }
+
+    @Override
+    public byte[] getContent()  {
+        try {
+            return Files.readAllBytes(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -37,13 +55,30 @@ public class FileResource implements LocalResource {
     }
 
     @Override
-    public long getLastModified() throws IOException {
-        return Files.getLastModifiedTime(path).toMillis();
+    public long getLastModified() {
+        try {
+            return Files.getLastModifiedTime(path).toMillis();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
-    public URL getURL() throws MalformedURLException {
-        return path.toUri().toURL();
+    public URL getURL(){
+        try {
+            return path.toUri().toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        try {
+            return Files.newInputStream(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 //<editor-fold desc = "XXXXXXX">
