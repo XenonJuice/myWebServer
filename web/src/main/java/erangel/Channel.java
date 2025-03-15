29a16ed114ac6a8b58b9fda6819e9baeb3077f94
LILINJIAN <shinozaki9852@gmail.com>
2025-03-15 23:38:58 +0900

    Add checkpoint management methods to Channel interface

--- web/src/main/java/erangel/Channel.java
+++ web/src/main/java/erangel/Channel.java
@@ -1,5 +1,44 @@
 package erangel;
 
+/**
+ * 表示一个在系统中管理检查点的通道。
+ * 一个通道可以设置、检索、添加或移除检查点。
+ */
 public interface Channel {
+    /**
+     * 获取与通道相关联的基本检查点。
+     *
+     * @return 与通道关联的基本 {@code CheckPoint} 实例，如果没有设置则返回 {@code null}。
+     */
+    CheckPoint getBasicCheckpoint();
 
+    /**
+     * 为通道设置基本检查点。
+     *
+     * @param checkpoint 要设置为基本检查点的检查点。
+     * @return 之前设置的基本检查点，如果没有设置检查点则返回 null。
+     */
+    CheckPoint setBasicCheckpoint(CheckPoint checkpoint);
+
+    /**
+     * 向通道添加一个检查点。
+     *
+     * @param checkpoint 要添加的检查点
+     */
+    void addCheckpoint(CheckPoint checkpoint);
+
+    /**
+     * 获取与当前通道相关联的所有检查点。
+     *
+     * @return 一个 {@code CheckPoint} 对象数组，表示所有检查点。
+     * 如果没有可用的检查点，则返回一个空数组。
+     */
+    CheckPoint[] getCheckpoint();
+
+    /**
+     * 从通道中移除指定的检查点。
+     *
+     * @param checkpoint 要移除的检查点。此检查点必须已经与通道相关联。
+     */
+    void removeCheckpoint(CheckPoint checkpoint);
 }
