687659821155ef7152afc05753fbaced57337911
LILINJIAN <shinozaki9852@gmail.com>
2025-03-16 21:20:46 +0900

    Add abstract CheckPointBase class as a base for checkpoints

---
+++ web/src/main/java/erangel/checkPoints/CheckPointBase.java
@@ -0,0 +1,27 @@
+package erangel.checkPoints;
+
+import erangel.base.CheckPoint;
+import erangel.base.CheckPointContext;
+import erangel.base.Vas;
+import erangel.base.VasManager;
+import erangel.connector.http.HttpRequest;
+import erangel.connector.http.HttpResponse;
+
+public abstract class CheckPointBase implements CheckPoint,VasManager {
+    //<editor-fold desc = "attr">
+    protected Vas vas = null;
+    //</editor-fold>
+    //<editor-fold desc = "一次实现">
+    @Override
+    public Vas getVas() {
+        return vas;
+    }
+
+    @Override
+    public void setVas(Vas vas) {
+        this.vas = vas;
+    }
+
+    public abstract void process(HttpRequest request, HttpResponse response, CheckPointContext context) throws Exception;
+    //</editor-fold>
+}
