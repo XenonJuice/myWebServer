d7d86bf15f169554f3be9fbfe17e6ee073f91d67
LILINJIAN <shinozaki9852@gmail.com>
2025-03-18 09:53:45 +0900

    Add getInfo method to Checkpoint interface

--- web/src/main/java/erangel/base/Checkpoint.java
+++ web/src/main/java/erangel/base/Checkpoint.java
@@ -4,5 +4,6 @@ import erangel.connector.http.HttpRequest;
 import erangel.connector.http.HttpResponse;
 
 public interface Checkpoint {
+    String getInfo();
     void process(HttpRequest request, HttpResponse response, CheckpointContext context) throws Exception;
 }
