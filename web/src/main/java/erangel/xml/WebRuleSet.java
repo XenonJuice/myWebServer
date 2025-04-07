a14d84b81cd59b5415104a14399558edf6d99aba
LILINJIAN <shinozaki9852@gmail.com>
2025-04-07 22:10:54 +0900

    Add rules for handling <servlet> and <display-name> elements

--- web/src/main/java/erangel/xml/WebRuleSet.java
+++ web/src/main/java/erangel/xml/WebRuleSet.java
@@ -1,8 +1,10 @@
 package erangel.xml;
 
 import erangel.core.DefaultContext;
+import erangel.core.DefaultEndpoint;
 import erangel.filter.FilterDef;
 import erangel.filter.FilterMap;
+import org.xml.sax.Attributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +12,17 @@ import java.util.List;
 public class WebRuleSet {
 
     public void addRuleInstances(MiniDigester d) {
-
-        /* 0. <web-app> → DefaultContext */
+        /* 0. <web-app> → DefaultContext 仅供测试*/
         d.addRule("web-app", new ObjectCreateRule(DefaultContext.class));
 
-        /* ---------- 1. <servlet‑mapping> ---------- */
+        /* ---------- <display‑name> ---------- */
+        d.addCallMethod("web-app/display-name", "setDisplayName");
+        /* ---------- <servlet> ---------- */
+        d.addRule("web-app/servlet", new endpointCreateRule());
+        d.addCallMethod("web-app/servlet/servlet-name", "setName");
+        d.addCallMethod("web-app/servlet/servlet-class", "setServletClass");
+        d.addRule("web-app/servlet", new SetNextRuleAccessible("addChild"));
+        /* ---------- <servlet‑mapping> ---------- */
         List<String> smArgs = new ArrayList<>();
         d.addRule("web-app/servlet-mapping/url-pattern", new CallParamRule(smArgs, 0));              // 收 url‑pattern
         d.addRule("web-app/servlet-mapping/servlet-name", new CallParamRule(smArgs, 1));              // 收 servlet‑name
@@ -23,20 +31,20 @@ public class WebRuleSet {
                 String.class,
                 String.class));
 
-        /* ---------- 2. <filter> ---------- */
+        /* ---------- <filter> ---------- */
         d.addRule("web-app/filter", new ObjectCreateRule(FilterDef.class));
         d.addCallMethod("web-app/filter/filter-name", "setFilterName");   // 单参数直接简写
         d.addCallMethod("web-app/filter/filter-class", "setFilterClass");
-        d.addRule("web-app/filter", new SetNextRule("addFilterDef"));
+        d.addRule("web-app/filter", new SetNextRuleAccessible("addFilterDef"));
 
-        /* ---------- 3. <filter‑mapping> ---------- */
+        /* ---------- <filter‑mapping> ---------- */
         d.addRule("web-app/filter-mapping", new ObjectCreateRule(FilterMap.class));
         d.addCallMethod("web-app/filter-mapping/filter-name", "setFilterName");
         d.addCallMethod("web-app/filter-mapping/servlet-name", "setServletName");
         d.addCallMethod("web-app/filter-mapping/url-pattern", "setUrlPattern");
-        d.addRule("web-app/filter-mapping", new SetNextRule("addFilterMap"));
+        d.addRule("web-app/filter-mapping", new SetNextRuleAccessible("addFilterMap"));
 
-        /* ---------- 4. <listener> ---------- */
+        /* ---------- <listener> ---------- */
         List<String> listenerArgs = new ArrayList<>();
         d.addRule("web-app/listener/listener-class", new CallParamRule(listenerArgs, 0));
         d.addRule("web-app/listener",
@@ -44,4 +52,15 @@ public class WebRuleSet {
                         listenerArgs,
                         String.class));
     }
+
+    private static class endpointCreateRule implements Rule {
+        @Override
+        public void begin(String path, Attributes attrs, MiniDigester d) {
+            // 栈底是外部 push 的 DefaultContext
+            DefaultContext ctx = d.peek(DefaultContext.class);
+            DefaultEndpoint endpoint = (DefaultEndpoint) ctx.createEndpoint();
+            d.push(endpoint);
+        }
+    }
+
 }
