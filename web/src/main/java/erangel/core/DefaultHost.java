5a6a830414391085350129112d8ebfed1123b402
LILINJIAN <shinozaki9852@gmail.com>
2025-04-11 17:38:17 +0900

    Fix typo in class name and implement DefaultHost functionality

---
+++ web/src/main/java/erangel/core/DefaultHost.java
@@ -0,0 +1,217 @@
+package erangel.core;
+
+import erangel.base.*;
+
+import java.io.IOException;
+import java.net.URL;
+
+import static erangel.base.Const.commonCharacters.SOLIDUS;
+
+public class DefaultHost extends VasBase implements Host, Host.Deployer {
+    // context对象
+    private final String defaultContextClass = "erangel.core.DefaultContext";
+    // 映射器对象 TODO
+    private final String defaultHostMapper = "erangel.core.DefaultHostMapper";
+    //<editor-fold desc = "attr">
+    // 程序根目录
+    private String appBase = "";
+    // 是否自动部署
+    private boolean autoDeploy = true;
+    // context容器监听器
+    private String innerContextListener = "erangel.listener.InnerContextListener";
+    // 工作区
+    private String workDir = "";
+    // 部署器
+    private Deployer deployer = new HostDeployer(this);
+
+    //</editor-fold>
+    //<editor-fold desc = "构造器">
+    public DefaultHost() {
+        // TODO 设置一个host的基本检查点
+        // channel.setBasicCheckpoint(XXX);
+    }
+
+    //</editor-fold>
+    //<editor-fold desc = "getter && setter">
+    public void setNamedHost(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWorkDir() {
+        return workDir;
+    }
+
+    public void setWorkDir(String workDir) {
+        this.workDir = workDir;
+    }
+
+    public boolean isAutoDeploy() {
+        return autoDeploy;
+    }
+
+    public void setAutoDeploy(boolean autoDeploy) {
+        this.autoDeploy = autoDeploy;
+    }
+
+    public String getInnerContextListener() {
+        return innerContextListener;
+    }
+
+    public void setInnerContextListener(String innerContextListener) {
+        this.innerContextListener = innerContextListener;
+    }
+
+    //</editor-fold>
+    //<editor-fold desc = "接口实现">
+    @Override
+    public Context map(String uri) {
+        logger.debug("Mapping uri:{}", uri);
+        if (uri == null) return null;
+        Context context;
+        String mappedUri = uri;
+        while (true) {
+            context = (Context) findChild(mappedUri);
+            if (context != null) break;
+            int index = mappedUri.lastIndexOf(SOLIDUS);
+            if (index == -1) break;
+            mappedUri = mappedUri.substring(0, index);
+        }
+        return context;
+    }
+
+    @Override
+    public String getAppBase() {
+        return appBase;
+    }
+
+    @Override
+    public void setAppBase(String appBase) {
+        this.appBase = appBase;
+    }
+
+    @Override
+    public void install(String contextPath, URL webApp) throws Exception {
+        deployer.install(contextPath, webApp);
+    }
+
+    @Override
+    public Context findDeployedApp(String contextPath) {
+        return deployer.findDeployedApp(contextPath);
+    }
+
+    @Override
+    public String[] findDeployedApps() {
+        return deployer.findDeployedApps();
+    }
+
+    @Override
+    public void remove(String contextPath) throws IOException {
+        deployer.remove(contextPath);
+    }
+
+    @Override
+    public void start(String contextPath) throws IOException {
+        deployer.start(contextPath);
+    }
+
+    @Override
+    public void stop(String contextPath) throws IOException {
+        deployer.stop(contextPath);
+    }
+
+    //</editor-fold>
+    //<editor-fold desc = "其他方法">
+    public void addChild(Vas child) {
+        if (!(child instanceof Context)) {
+            throw new IllegalArgumentException("child must be a context");
+        }
+        super.addChild(child);
+    }
+
+    private void beforeInstall(String contextPath, URL webApp) {
+        if (contextPath == null || webApp == null) throw new IllegalArgumentException(
+                "contextPath and webApp must not be null"
+        );
+        if (!contextPath.isEmpty() && !contextPath.startsWith(SOLIDUS)) throw new IllegalArgumentException(
+                "contextPath must start with /"
+        );
+        if (findDeployedApp(contextPath) != null) throw new IllegalArgumentException(
+                "contextPath has been deployed"
+        );
+    }
+
+    //</editor-fold>
+    //<editor-fold desc = "内部类">
+    public class HostDeployer implements Deployer {
+        private DefaultHost host = null;
+
+        public HostDeployer(DefaultHost host) {
+            this.host = host;
+        }
+
+        @Override
+        public void install(String contextPath, URL webApp) throws Exception {
+            beforeInstall(contextPath, webApp);
+            logger.debug("Installing web app:{} from :{}", webApp, contextPath);
+            String webAppUrl = webApp.toString();
+            String basePath = null;
+            logger.debug("webAppUrl:{}", webAppUrl);
+            if (webAppUrl.startsWith("file:")) {
+                basePath = webAppUrl.substring(5);
+            } else if (webAppUrl.startsWith("file://")) {
+                basePath = webAppUrl.substring(7);
+            } else {
+                throw new IllegalArgumentException("webAppUrl must start with file:");
+            }
+
+            try {
+                // 创建 Context对象
+                Class<? extends Context> typedContextClass = Class.forName(defaultContextClass).asSubclass(Context.class);
+                Context contextInstance = typedContextClass.getDeclaredConstructor().newInstance();
+                contextInstance.setPath(contextPath);
+                contextInstance.setBasePath(basePath);
+
+                // 创建 InnerContextListener对象
+                Class<?> listener = Class.forName(innerContextListener);
+                LifecycleListener listenerInstance = (LifecycleListener) listener.getDeclaredConstructor().newInstance();
+                // 设置监听器
+                ((Lifecycle) contextInstance).addLifecycleListener(listenerInstance);
+                // 将当前Context作为子容器注入当前关联的Host
+                host.addChild(contextInstance);
+            } catch (Exception e) {
+                logger.error("Deploy web app error", e);
+                throw new Exception("Deploy web app error", e);
+            }
+        }
+
+        @Override
+        public Context findDeployedApp(String contextPath) {
+            return null;
+        }
+
+        @Override
+        public String[] findDeployedApps() {
+            return new String[0];
+        }
+
+        @Override
+        public void remove(String contextPath) throws IOException {
+
+        }
+
+        @Override
+        public void start(String contextPath) throws IOException {
+
+        }
+
+        @Override
+        public void stop(String contextPath) throws IOException {
+
+        }
+    }
+    //</editor-fold>
+}
