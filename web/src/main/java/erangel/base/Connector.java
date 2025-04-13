4eeed7bfec38c2e2471427f5cd99fd114e520245
LILINJIAN <shinozaki9852@gmail.com>
2025-04-13 23:55:52 +0900

    Add Connector interface to define base structure

---
+++ web/src/main/java/erangel/base/Connector.java
@@ -0,0 +1,9 @@
+package erangel.base;
+/**
+ *  To make the abstract structure look more aesthetically pleasing,
+ *  this interface has been declared.
+ *  Currently, there is only one HTTP connector.<code>:-)</code>
+ * */
+public interface Connector {
+
+}
