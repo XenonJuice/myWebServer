8cc4eb4550068f217163f4afc773449d5369faff
LILINJIAN <shinozaki9852@gmail.com>
2025-04-15 17:59:05 +0900

    Add methods to get and set default host name in Engine

--- web/src/main/java/erangel/base/Engine.java
+++ web/src/main/java/erangel/base/Engine.java
@@ -20,4 +20,19 @@ public interface Engine extends Vas {
      * @return String 表示描述信息
      */
     String getInfo();
+
+    /**
+     * 检索与引擎关联的默认主机名。
+     *
+     * @return 一个 {@code String}，表示默认主机名，
+     * 如果未配置默认主机，则返回 {@code null}。
+     */
+    String getDefaultHostName();
+
+    /**
+     * 设置此引擎的默认主机名。默认主机名决定了当没有匹配的特定主机名时，哪个子主机将处理请求。
+     *
+     * @param name 本引擎使用的默认主机名。它应对应于某个子主机的名称。
+     */
+    void setDefaultHostName(String name);
 }
