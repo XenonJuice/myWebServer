aa9e9ee4c2e836a931c9eb796ee4185757e7946b
LILINJIAN <shinozaki9852@gmail.com>
2025-04-15 17:55:49 +0900

    Add UTF-8 overloaded decode method in Decoder

--- web/src/main/java/erangel/utils/Decoder.java
+++ web/src/main/java/erangel/utils/Decoder.java
@@ -1,9 +1,13 @@
 package erangel.utils;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Decoder {
     public static String decode(String value, Charset charset) {
         return java.net.URLDecoder.decode(value, charset);
+    }
+    public static String decode(String value) {
+        return java.net.URLDecoder.decode(value, StandardCharsets.UTF_8);
     }
 }
