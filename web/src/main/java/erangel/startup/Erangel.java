ce834b4ea902a02c5990a096fbdb3446c1bb2b4b
LILINJIAN <shinozaki9852@gmail.com>
2025-04-23 20:59:39 +0900

    Add empty Erangel class as a startup placeholder

---
+++ web/src/main/java/erangel/startup/Erangel.java
@@ -0,0 +1,16 @@
+package erangel.startup;
+
+public class Erangel {
+    //<editor-fold desc = "XXXXXXX">
+    //</editor-fold>
+    //<editor-fold desc = "XXXXXXX">
+    //</editor-fold>
+    //<editor-fold desc = "XXXXXXX">
+    //</editor-fold>
+    //<editor-fold desc = "XXXXXXX">
+    //</editor-fold>
+    //<editor-fold desc = "XXXXXXX">
+    //</editor-fold>
+    //<editor-fold desc = "XXXXXXX">
+    //</editor-fold>
+}
