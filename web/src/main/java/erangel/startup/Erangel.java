d894ba691431f0f161cfab26474717b1f6063cd1
LILINJIAN <shinozaki9852@gmail.com>
2025-05-20 17:53:47 +0900

    Add command-line argument parsing and help messages

--- web/src/main/java/erangel/startup/Erangel.java
+++ web/src/main/java/erangel/startup/Erangel.java
@@ -1,9 +1,25 @@
 package erangel.startup;
 
+import java.io.File;
+
 import static erangel.base.Const.confInfo.*;
 
 public class Erangel {
     //<editor-fold desc = "配置二进制目录和实例运行目录">
+
+    private static final String HELP_MESSAGE =
+            """
+                    Usage: Erangel
+                      -c,--config     Server.XML file path
+                      -h,--help       Show this help message
+                      -d,--debug      Debug mode
+                      -v,--version    Show version information
+                      start           Start the server
+                      stop            Stop the server""";
+    //</editor-fold>
+    //<editor-fold desc = "attr">
+    private boolean debugMode = false;
+    private String serverXMLPath = null;
 
     /**
      * 通过设置 {@code CORE_DIR} 系统属性来配置应用程序的核心目录。
@@ -28,15 +44,39 @@ public class Erangel {
             System.setProperty(DEPLOY_DIR, System.getProperty(USER_DIR));
         }
     }
+
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "运行">
+    public void process(String[] args) {
+        // 设置服务器核心目录
+        setCoreDir();
+        // 设置实例部署目录
+        setInstanceDir();
+    }
+
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "读取输入的命令以设置flag">
+    private boolean readCommand(String[] args) {
+        if (args == null || args.length == 0) help();
+        return false;
+    }
+
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+    //<editor-fold desc = "说明信息>
+    private void help() {
+        System.out.println(HELP_MESSAGE);
+    }
     //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
-    //</editor-fold>
-    //<editor-fold desc = "XXXXXXX">
+
+    //<editor-fold desc = "服务器配置文件">
+    private File serverXML() {
+        File file = new File(serverXMLPath);
+        if (!file.exists() || !file.isFile() || !file.canRead()
+                || !file.isAbsolute()) {
+            file = new File(System.getProperty(DEPLOY_DIR), serverXMLPath);
+        }
+        return file;
+    }
     //</editor-fold>
 }
+
