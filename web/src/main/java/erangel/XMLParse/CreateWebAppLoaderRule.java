053e5a27d2a15d4070e10615b3681df26b1ffcc2
LILINJIAN <shinozaki9852@gmail.com>
2025-05-29 17:35:22 +0900

    Refactor WebAppLoader creation logic in CreateWebAppLoaderRule

--- web/src/main/java/erangel/XMLParse/CreateWebAppLoaderRule.java
+++ web/src/main/java/erangel/XMLParse/CreateWebAppLoaderRule.java
@@ -17,13 +17,13 @@ public class CreateWebAppLoaderRule implements Rule {
     public void begin(String path, Attributes attrs, MiniDigester d) {
         // 取出当前容器（即为Context）
         Vas vas = digester.peek();
-        // 反射调用 vas.setLoader(loader)
+        // 反射创建一个WebAppLoader
         try {
             Class<?> loaderClass = Class.forName(loaderClassName);
             // 取得WebAppLoader构造函数需要的参数类型
             Class<?> type = ClassLoader.class;
             // 取得容器的父容器的类加载器
-            Object[] arg = {vas.getParentClassLoader()};
+            ClassLoader arg = vas.getParentClassLoader();
             // 取得方法
             // Method m = vas.getClass().getMethod("setLoader", Loader.class);
             // 实例化
